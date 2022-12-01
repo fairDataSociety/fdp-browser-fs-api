4095e13084cca091492e5b2fdf1d7538e9bed69b
Rogelio Morrell <molekilla@gmail.com>
2022-12-01 18:37:07 -0500

    feat:  adds list  and upload

--- test/browser/src/App.js
+++ test/browser/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { FSBrowser } from './fsbrowser'
 import { FairdriveBrowser } from './fairdrive'
 
-import {FdpStorage} from '@fairdatasociety/fdp-storage'
+import { FdpStorage } from '@fairdatasociety/fdp-storage'
 
+export const GLOBAL_POSTAGE_BATCH_ID = '54ed0da82eb85ab72f9b8c37fdff0013ac5ba0bf96ead71d4a51313ed831b9e5'
 
-export const GLOBAL_POSTAGE_BATCH_ID =
-  "0000000000000000000000000000000000000000000000000000000000000000";
+import { FdpContracts } from '@fairdatasociety/fdp-storage'
+// TODO temporary because the fdp-storage is not exporting these values
+const { Environments, getEnvironmentConfig } = FdpContracts || {}
 
-  export function getEnsConfig() {
-  const ensRegistry = process.env.REACT_APP_ENS_REGISTRY_ADDRESS;
-  const publicResolver = process.env.REACT_APP_PUBLIC_RESOLVER_ADDRESS;
-  const fdsRegistrar = process.env.REACT_APP_SUBDOMAIN_REGISTRAR_ADDRESS;
-  const rpcUrl = process.env.REACT_APP_RPC_URL;
-  const ensDomain = "fds";
+function getEnvironment() {
+  const environment = process.env.REACT_APP_ENVIRONMENT
+
+  if (environment === 'LOCALHOST') {
+    return getEnvironmentConfig(Environments.LOCALHOST)
+  } else if (environment === 'GOERLI') {
+    return getEnvironmentConfig(Environments.GOERLI)
+  }
+
+  return undefined
+}
+
+export function getEnsConfig() {
+  let ensOptions = getEnvironment()
+  const ensRegistry = process.env.REACT_APP_ENS_REGISTRY_ADDRESS
+  const publicResolver = process.env.REACT_APP_PUBLIC_RESOLVER_ADDRESS
+  const fdsRegistrar = process.env.REACT_APP_SUBDOMAIN_REGISTRAR_ADDRESS
+  const rpcUrl = process.env.REACT_APP_RPC_URL
+  const ensDomain = 'fds'
 
   if (!rpcUrl && !ensRegistry && !publicResolver && !fdsRegistrar) {
     return ensOptions
@@ -23,13 +38,13 @@ export const GLOBAL_POSTAGE_BATCH_ID =
           ensOptions,
           ensDomain,
         }
-      : undefined;
+      : undefined
   }
 
-  let ensOptions = {};
+  ensOptions = ensOptions || {}
 
   if (rpcUrl) {
-    ensOptions.rpcUrl = rpcUrl;
+    ensOptions.rpcUrl = rpcUrl
   }
 
   if (ensRegistry && publicResolver && fdsRegistrar) {
@@ -37,7 +52,7 @@ export const GLOBAL_POSTAGE_BATCH_ID =
       ensRegistry,
       publicResolver,
       fdsRegistrar,
-    };
+    }
   }
 
   return {
@@ -46,20 +61,19 @@ export const GLOBAL_POSTAGE_BATCH_ID =
       ...ensOptions,
     },
     ensDomain,
-  };
-}
-
-
-const fdp = new FdpStorage(process.env.REACT_APP_BEE_URL, GLOBAL_POSTAGE_BATCH_ID, getEnsConfig())
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App" style={{ display: 'flex' }}>
-        <FairdriveBrowser name="Fairdrive" id="fairdrive" fdp={fdp} />
-      </div>
-    )
   }
+}
+const client = new FdpStorage(
+  process.env.REACT_APP_BEE_URL,
+  process.env.REACT_APP_GLOBAL_BATCH_ID,
+  getEnsConfig(),
+)
+const App = () => {
+  return (
+    <div className="App" style={{ display: 'flex' }}>
+      <FairdriveBrowser name="Fairdrive" id="fairdrive" fdp={client} />
+    </div>
+  )
 }
 
 export default App
--- test/browser/src/fairdrive.jsx
+++ test/browser/src/fairdrive.jsx
@@ -1,46 +1,136 @@
 import React, { Component } from 'react'
 import { FullFileBrowser } from 'chonky'
-import { getOriginPrivateDirectory, support } from 'native-file-system-adapter'
+import { getOriginPrivateDirectory, showOpenFilePicker, showSaveFilePicker } from 'native-file-system-adapter'
 import { useEffect } from 'react'
 import { ChonkyActions } from 'chonky'
+import { useCallback } from 'react'
 
 export const FairdriveBrowser = ({ fdp, id, name }) => {
-  const files = []
+  const [items, setItems] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+  const [podItem, setPod] = React.useState({ name: '' })
   const folderChain = [{ id, name, isDir: true }]
   const myFileActions = [ChonkyActions.UploadFiles, ChonkyActions.DownloadFiles, ChonkyActions.DeleteFiles]
   useEffect(() => {
     async function getHandle() {
+      setLoading(true)
       const adapter = await import('./dist/index.js')
+      await fdp.account.login(process.env.REACT_APP_USERNAME, process.env.REACT_APP_PASSWORD)
 
-      let handle
-      await fdp.account.login(`testing`, `abtesting`)
-      
-      handle = await getOriginPrivateDirectory(adapter, {
-        fdp,
-        podname: `root`,
-        id: `54ed0da82eb85ab72f9b8c37fdff0013ac5ba0bf96ead71d4a51313ed831b9e5`,
-        path: '/',
-        reference: `testing`,
-      })
+      let pods
+      try {
+       pods = await fdp.personalStorage.list() //(`testing-${Date.now()}`)
+      } catch (e) {
+        console.log(e)
+      }
+      if (pods) {
+        const pod = pods.getPods().reverse()[8]
+        console.log(`Using pod ${pod.name}`)
+        const rootHandle = await getOriginPrivateDirectory(adapter, {
+          fdp,
+          podname: pod.name,
+          path:'/'
+        })
+        const files = []
 
-      const entries = await handle.entries()
-
-      let entry = await entries.next()
-      while (!entry.done) {
-        console.log(entry.value)
-        entry = await entries.next()
-        if (entry[1].kind === 'directory') {
-          files.push({ id: entry[0], name: entry[0], isDir: true, handle: entry[1] })
-        } else {
-          files.push({ id: entry[0], name: entry[0], isDir: false, handle: entry[1] })
+        for await (let [name, entry] of rootHandle.entries()) {
+          if (entry.kind === 'directory') {
+            const item = { id: name, name: name, isDir: true, handle: entry }
+            files.push(item)
+          } else {
+            const item = { id: name, name: name, isDir: false, handle: entry }
+            files.push(item)
+          }
         }
+
+        setPod({ ...pod })
+        setItems(files)
+        setLoading(false)
       }
     }
     getHandle()
   }, [])
+
+  const handleAction = podItem =>
+    useCallback(
+      data => {
+        async function upload() {
+          // request user to select a file
+          const [picker] = await showOpenFilePicker({
+            types: [], // default
+            multiple: false, // default
+            excludeAcceptAllOption: false, // default
+            _preferPolyfill: false, // default
+          })
+
+          // returns a File Instance
+          const file = await picker.getFile()
+          const adapter = await import('./dist/index.js')
+
+          // copy the file over to a another place
+          const rootHandle = await getOriginPrivateDirectory(adapter, {
+            fdp,
+
+            path:'/',
+            podname: podItem.name,
+          })
+          const fileHandle = await rootHandle.getFileHandle(file.name, { create: true })
+          const writable = await fileHandle.createWritable({ keepExistingData: false })
+          await writable.write(file)
+          await writable.close()
+        }
+
+        async function download() {
+          const fileHandle = await showSaveFilePicker({
+            _preferPolyfill: true,
+            suggestedName: 'Untitled.png',
+            types: [
+              { accept: { 'image/png': ['png'] } },
+              { accept: { 'image/jpg': ['jpg'] } },
+              { accept: { 'image/webp': ['webp'] } },
+            ],
+            excludeAcceptAllOption: false, // default
+          })
+
+          // Look at what extension they chosen
+          const extensionChosen = fileHandle.name.split('.').pop()
+
+          const blob = {
+            jpg: generateCanvasBlob({ type: 'blob', format: 'jpg' }),
+            png: generateCanvasBlob({ type: 'blob', format: 'png' }),
+            webp: generateCanvasBlob({ type: 'blob', format: 'webp' }),
+          }[extensionChosen]
+
+          const writer = await fileHandle.createWritable()
+          await writer.write(blob)
+          await writer.close()
+        }
+
+        async function deleteFile() {
+          await data.file.handle.removeEntry()
+        }
+        setLoading(true)
+
+        if (data.id === ChonkyActions.UploadFiles.id) {
+          upload()
+        } else if (data.id === ChonkyActions.DownloadFiles.id) {
+          download()
+        } else if (data.id === ChonkyActions.DeleteFiles.id) {
+          deleteFile()
+        }
+        setLoading(false)
+      },
+      [podItem],
+    )
   return (
-    <div style={{ height: 600, width: 300, flex: '50%' }}>
-      <FullFileBrowser files={files} folderChain={folderChain} fileActions={myFileActions} />
+    <div style={{ height: 600, width: 300, flex: '100%' }}>
+      {loading ? <div>Loading...</div> : null}
+      <FullFileBrowser
+        onFileAction={handleAction(podItem)}
+        files={items}
+        folderChain={folderChain}
+        fileActions={myFileActions}
+      />
     </div>
   )
 }
